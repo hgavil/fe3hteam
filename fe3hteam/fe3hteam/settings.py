80ea31ea1604a9824c37cb2aeddc1e951cee2166
Hannah <hgavilan15@gmail.com>
2023-03-13 18:18:03 +0000

    fixed settings

--- fe3hteam/fe3hteam/settings.py
+++ fe3hteam/fe3hteam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x5o7$+0e2bc1uoq5vvt@f9%=72)d+hnzyop286(h*qlpqpv0p$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
